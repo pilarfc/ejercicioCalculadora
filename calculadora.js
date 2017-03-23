78f6559c08dfcf2ed464e93b8437a2aef1bf0908
Pilar Figueroa <pilar.fc@outlook.com>
2017-03-23 13:35:49 -0600

    Calculadora completa

--- calculadora.js
+++ calculadora.js
@@ -2,12 +2,22 @@
 
 function mostrarNumero (num){
   var guardarNumero = document.formulario.resultado.value;
-  document.formulario.resultado.value = guardarNumero + num;
+   document.formulario.resultado.value = guardarNumero + num;
 }
 
 
 function signoIgual (valor){
 
  var numeroPresionado = document.getElementById("textoPantalla");
- numeroPresionado.value = eval(numeroPresionado.value);
+  numeroPresionado.value = eval(numeroPresionado.value);
+}
+
+function teclaPresionada (event) {
+  var codigoASCII = event.keyCode;
+    if (codigoASCII == 13){ // 13 codigoascii enter
+     signoIgual();
+     return false;
+}    else if (codigoASCII < 42 || codigoASCII > 57) {
+      return false;
+  }
 }
