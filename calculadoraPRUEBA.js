bf188c8db3d26f70805cdd5498950c68ca7899a3
Pilar Figueroa <pilar.fc@outlook.com>
2017-03-23 12:39:40 -0600

    Primer commit

--- calculadoraPRUEBA.js
+++
@@ -1,9 +0,0 @@
-
-
-function operaciones(operacion){
-    var operacion1 = document.formulario.operacion1.value
-    var operacion2 = document.formulario.operacion2.value
-    var resultado = eval(operacion1 + operacion + operacion2)
-    document.formulario.resultado.value = resultado
-
-}
